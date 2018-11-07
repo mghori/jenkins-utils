bc64b8af2f0a5f57c4652f5fb81ef46078a45c44
Mariam Ghori <ghori_m@yahoo.com>
2018-11-07 10:23:23 -0800

    smoke test

--- src/gov/ca/cwds/jenkins/SmokeTest.groovy
+++ src/gov/ca/cwds/jenkins/SmokeTest.groovy
@@ -1,9 +1,18 @@
 package gov.ca.cwds.jenkins
 
 class SmokeTest {
+  def path
+
+  SmokeTest(path) {
+    this.path = path
+  }
   
-  def runSmokeTest() {
-    def test = sh (script: './test/smoketest.sh',  returnStdout: true)
+  def runSmokeTest(path) {
+    echo "smoketest"
+    echo "${workspace}
+    echo "${path}
+    def test = sh (script: path,  returnStdout: true)
+    echo "Test: ${test}"
     if (test.contains("smoketest passed")) {
           throw new Exception("'${test}'")
     }
