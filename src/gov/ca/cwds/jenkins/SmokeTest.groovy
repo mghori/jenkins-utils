4a15d11318869e644bb84b8f9cc2347c04da6060
Mariam Ghori <ghori_m@yahoo.com>
2018-11-08 11:35:02 -0800

    smoke test

--- src/gov/ca/cwds/jenkins/SmokeTest.groovy
+++ src/gov/ca/cwds/jenkins/SmokeTest.groovy
@@ -10,7 +10,7 @@ class SmokeTest {
   def runSmokeTest(path) {
      def test = path.execute().text
     if (test.contains("smoketest failed")) {
-        throw new Exception("'${test}'")
+        error ("'${test}'")
     }
   }
   
