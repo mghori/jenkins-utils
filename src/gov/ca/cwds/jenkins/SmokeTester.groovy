83f180d32dd09fe20c1a9006493291dc9f735fb7
Mariam Ghori <ghori_m@yahoo.com>
2018-11-14 10:29:02 -0800

    smoke test

--- src/gov/ca/cwds/jenkins/SmokeTester.groovy
+++ src/gov/ca/cwds/jenkins/SmokeTester.groovy
@@ -9,10 +9,11 @@ class SmokeTester {
   
   def runSmokeTest(path) {
     
-    def cmd = [path, "http://dashboard:8888/system-information"]
+    def cmd = [path, script.SMOKE_TEST_URL]
     def test = cmd.execute().text
     script.echo "TEST:'${test}'"
     script.echo script.SMOKE_TEST_URL
+    script.echo "TEST:'${test}'"
     if (test.contains("smoketest failed")) {
         script.error ("'${test}'")
         return "smoke test failed"
