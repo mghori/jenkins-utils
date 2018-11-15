1f955c31b5e88666282a9aa69260a26f02a9b377
Mariam Ghori <ghori_m@yahoo.com>
2018-11-15 10:54:29 -0800

    smoketest

--- src/gov/ca/cwds/jenkins/SmokeTester.groovy
+++ src/gov/ca/cwds/jenkins/SmokeTester.groovy
@@ -9,8 +9,8 @@ class SmokeTester {
   
   def runSmokeTest(path, url) {
     script.echo "${path}"
-    script.echo " ${url}"
-    def test = script.sh (script: '"${path}" "${url}"', returnStdout: true).trim()
+    script.echo "${path} ${url}"
+    def test = script.sh (script: "${path} ${url}", returnStdout: true).trim()
     if (test.contains("smoketest passed")) {
         script.echo "smoke test passed"
         return "smoke test passed"
