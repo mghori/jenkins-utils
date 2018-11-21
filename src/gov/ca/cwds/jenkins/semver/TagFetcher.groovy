49f26057af76ca498cdda2fb14aedb995fd285fc
Mariam Ghori <ghori_m@yahoo.com>
2018-11-20 22:37:21 -0800

    semver

--- src/gov/ca/cwds/jenkins/semver/TagFetcher.groovy
+++ src/gov/ca/cwds/jenkins/semver/TagFetcher.groovy
@@ -9,7 +9,10 @@ class TagFetcher {
 
   def getTags() {
     script.echo "getTags"
+    def pwd = script.sh(script: "pwd", returnStdout: true)
+    script.echo pwd
     def rawTags = script.sh(script: "git tag", returnStdout: true)
+    script.echo rawTags
     def list = rawTags.split("\n").findAll { it =~ /(^\d+\.\d+\.\d+)/ }
     list.collect { tag ->
        (tag =~ /(^\d+\.\d+\.\d+)/).with { it.hasGroup() ? it[0][0] : null }
